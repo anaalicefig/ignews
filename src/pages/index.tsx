56c8ec4cffb79083026bd0783ffafe582f5b3a60
Ana Figueiredo <anaalicefm22@gmail.com>
2022-06-22 15:49:43 -0300

    feat: creating header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
+import Head from 'next/head';
+
 export default function Home() {
   return (
-    <h1>hello world</h1>
+    <>
+      <Head>
+        <title>home ignews</title>
+      </Head>
+      <h1>hello world</h1>
+    </>
   )
 }
